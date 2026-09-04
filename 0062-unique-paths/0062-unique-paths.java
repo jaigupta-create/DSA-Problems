856d1b074677d1f78840a57b36042c113c9c313d
jaigupta-create <jaigupta60000@gmail.com>
2026-09-04 09:21:34 +0530

    Time: 0 ms (100%), Space: 42.1 MB (65.74%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int uniquePaths(int m, int n) {
+        int[] prev=new int[n];
+        for(int i=0;i<m;i++){
+            int[] temp=new int[n];
+            for(int j=0;j<n;j++){
+                if(i==0 || j==0) temp[j]=1;
+                else temp[j]=prev[j]+temp[j-1];
+            }
+            prev=temp;
+        }
+        return prev[n-1];
+    }
+}
